f31929829cfaa323588e1118dd3e7cc15ace0b2b
Murad Hossain <murad@tajtechltd.com>
2023-09-03 11:20:01 +0600

    user sign up with password hassing

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,3 +1,4 @@
+import bcrypt from 'bcrypt';
 import { Request, Response } from 'express';
 import httpStatus from 'http-status';
 import catchAsync from '../../../shared/catchAsync';
@@ -5,7 +6,27 @@ import sendResponse from '../../../shared/sendResponse';
 import { UserService } from './user.service';
 
 const insertIntoDB = catchAsync(async (req: Request, res: Response) => {
-  const result = await UserService.insertIntoDB(req.body);
+  const { id, name, email, password, role, contactNo, address, profileImg } =
+    req.body;
+
+  // Hash the user's password
+  const hashedPassword = await bcrypt.hash(password, 10); // 10 is the number of bcrypt rounds
+
+  // Create a user object with the hashed password
+  const userData = {
+    id,
+    name,
+    email,
+    password: hashedPassword,
+    role,
+    contactNo,
+    address,
+    profileImg,
+  };
+
+  const result = await UserService.insertIntoDB(userData);
+
+  //   const result = await UserService.insertIntoDB(req.body);
   sendResponse(res, {
     statusCode: httpStatus.OK,
     success: true,
--- src/app/modules/user/user.interface.ts
+++ src/app/modules/user/user.interface.ts
@@ -1,5 +1,4 @@
 export type IUser = {
-  id: string;
   name: string;
   email: string;
   password: string;
