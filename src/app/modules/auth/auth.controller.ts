07f909a58c8472e2db1cb9c9dfb4384a29f9233c
Murad Hossain <murad@tajtechltd.com>
2023-09-03 12:56:07 +0600

    login token with user role and get all users only for admin - DONE

--- src/app/modules/auth/auth.controller.ts
+++ src/app/modules/auth/auth.controller.ts
@@ -1,8 +1,8 @@
-// auth.controller.ts
-
 import bcrypt from 'bcrypt';
 import { Request, Response } from 'express';
-import jwt from 'jsonwebtoken';
+import { Secret } from 'jsonwebtoken';
+import config from '../../../config';
+import { jwtHelpers } from '../../../helpers/jwtHelpers';
 import { AuthService } from './auth.service';
 
 const loginUser = async (req: Request, res: Response) => {
@@ -18,19 +18,26 @@ const loginUser = async (req: Request, res: Response) => {
 
     // Check if the entered password matches the stored hashed password
     const passwordMatch = await bcrypt.compare(password, user.password);
-
+    console.log('m: ' + passwordMatch);
     if (!passwordMatch) {
-      return res.status(401).json({ message: 'Authentication failed' });
+      return res.status(401).json({ message: 'Password Not match' });
     }
 
     // Generate a JWT token for the authenticated user
-    const token = jwt.sign({ userId: user.id }, 'your-secret-key', {
-      expiresIn: '1h',
-    });
+    const { id: userId, role } = user;
+    const accessToken = jwtHelpers.createToken(
+      { userId, role },
+      config.jwt.secret as Secret,
+      config.jwt.expires_in as string
+    );
+
+    // const token = jwt.sign({ userId: user.id }, 'your-secret-key', {
+    //   expiresIn: '1h',
+    // });
 
     res.status(200).json({
       message: 'Authentication successful',
-      token,
+      accessToken,
     });
   } catch (error) {
     console.error(error);
--- src/app/modules/user/user.routes.ts
+++ src/app/modules/user/user.routes.ts
@@ -1,11 +1,11 @@
 import express from 'express';
-// import { ENUM_USER_ROLE } from '../../../enums/user';
-// import auth from '../../middlewares/auth';
 // import validateRequest from '../../middlewares/validateRequest';
+import { ENUM_USER_ROLE } from '../../../enums/user';
+import auth from '../../middlewares/auth';
 import { UserController } from './user.controller';
 
 const router = express.Router();
 
-router.get('/', UserController.getAllUsers);
+router.get('/', auth(ENUM_USER_ROLE.ADMIN), UserController.getAllUsers);
 
 export const userRoutes = router;
